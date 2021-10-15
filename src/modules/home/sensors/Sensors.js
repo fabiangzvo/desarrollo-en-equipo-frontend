c70226ffb88c19e056f586e8bbffd5aa526ef409
juanmalay <56891160+juanmalay@users.noreply.github.com>
2021-10-15 16:04:13 -0500

    Se ajustaron las notificaciones

--- src/modules/home/sensors/Sensors.js
+++ src/modules/home/sensors/Sensors.js
@@ -3,60 +3,64 @@ import { Switch, notification } from "antd";
 
 
 import api from "@/api/sensor";
-import { StyledContainer, Robot,StyledContainer3,StyledContainer4,Titulo2,StyledContainer5,Titulo,Contenedor,Fondo,Fondo2,StyledContainer6,} from './Sensors.style';
+import { StyledContainer, Robot, StyledContainer3, StyledContainer4, Titulo2, StyledContainer5, Titulo, Contenedor, Fondo, Fondo2, StyledContainer6, } from './Sensors.style';
 function Sensors() {
 
-  async function onChange(value) {
-    const response = await api.changeLightStatus(value)
 
+
+  async function onChange(value) {
+    console.log("Entro")
+    const response = await api.changeLightStatus(value)
+    
+    console.log(response)
     if (response.status !== 200) return notification.warning({ message: 'No se ha podido cambiar el estado de las luces' })
 
     return notification.success({ message: `las luces se han ${value ? 'encendido' : 'apagado'}` })
   }
- 
-    
+
+
 
   return (
     <div>
       <StyledContainer5>
-    <StyledContainer>
-    <Robot src="http://localhost:3000/robot.png" />
-        <Titulo>SMART HOME</Titulo>
-        
-    </StyledContainer>
-   
-    <StyledContainer3>
-    <Fondo src="http://localhost:3000/planta1.png"/>
-      
-      <Contenedor>
-     
-      <Titulo2>Interruptores</Titulo2>
-        
-      <StyledContainer6>
-      <Switch efaultChecked  onChange={onChange} /> Bombillo 1
-    </StyledContainer6>
+        <StyledContainer>
+          <Robot src="http://localhost:3000/robot.png" />
+          <Titulo>SMART HOME</Titulo>
 
-    <StyledContainer6>
-      <Switch efaultChecked  onChange={onChange} /> Bombillo 2
-    </StyledContainer6>
-    
-    <StyledContainer6>
-      <Switch efaultChecked  onChange={onChange} /> Bombillo 3
-    </StyledContainer6>
-   
-    <StyledContainer6>
-      <Switch efaultChecked  onChange={onChange} /> Bombillo 4
-    </StyledContainer6>
+        </StyledContainer>
 
-      </Contenedor>
-    </StyledContainer3>
+        <StyledContainer3>
+          <Fondo src="http://localhost:3000/planta1.png" />
 
-    
-    <StyledContainer4>
-   <Fondo2 src="http://localhost:3000/Convenciones.png"/>
-    </StyledContainer4>
-</StyledContainer5>
-     
+          <Contenedor>
+
+            <Titulo2>Interruptores</Titulo2>
+
+            <StyledContainer6>
+              <Switch onChange={onChange} /> Bombillo 1
+            </StyledContainer6>
+
+            <StyledContainer6>
+              <Switch onChange={onChange} /> Bombillo 2
+            </StyledContainer6>
+
+            <StyledContainer6>
+              <Switch onChange={onChange} /> Bombillo 3
+            </StyledContainer6>
+
+            <StyledContainer6>
+              <Switch onChange={onChange} /> Bombillo 4
+            </StyledContainer6>
+
+          </Contenedor>
+        </StyledContainer3>
+
+
+        <StyledContainer4>
+          <Fondo2 src="http://localhost:3000/Convenciones.png" />
+        </StyledContainer4>
+      </StyledContainer5>
+
     </div>
   )
 
